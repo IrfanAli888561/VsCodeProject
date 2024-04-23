73e9c23f111da4c30f8b42055e38ccdd161d56cf
Irfan Ali <irfanali888561@gmail.com>
2024-04-23 14:01:31 +0500

    All files has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'First Page';
+  DisplayVal = '';
+  color = "red";
+  count = 0;
+  userData:any
+  
+  getName(val:string)
+  {
+    alert(val)
+  }
+  getValue(val:string)
+  {
+    console.warn(val)
+    this.DisplayVal = val
+  }
+  counter(){
+    this.count ++;
+  }
+  counter1(){
+    this.count --;
+  }
+updateColor(){
+  this.color = "blue";
+}
+getData(data:any)
+{
+  console.warn(data)
+  this.userData = data;
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { UserAuthModule } from './user-auth/user-auth.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserAuthModule,
+    FormsModule
   ],
+  
   providers: [
     provideClientHydration()
   ],
